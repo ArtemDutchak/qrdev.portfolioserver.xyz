e51a3f1268327cef7dfa37ad4e95a9345336f703
ArtemDutchak <artemdutchak@gmail.com>
2022-09-14 14:47:42 +0300

    add reviews filtering translation

---
+++ catalog/language/en-gb/account/reviews.php
@@ -0,0 +1,8 @@
+<?php
+
+// Text
+$_['text_to_filter']  = 'Filter:';
+
+$_['text_more_three'] = 'More than three';
+$_['text_less_four']  = 'Less than four';
+$_['text_solved']     = 'Solved';
---
+++ catalog/language/uk-ua/account/reviews.php
@@ -0,0 +1,8 @@
+<?php
+
+// Text
+$_['text_to_filter']  = 'Фільтрувати:';
+
+$_['text_more_three'] = 'Більше трьох';
+$_['text_less_four']  = 'Менше чотирьох';
+$_['text_solved']     = 'Вирішені';
