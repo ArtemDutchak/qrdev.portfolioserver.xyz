503f4c4be06739cc37f42eb9ec158dc899c5a338
ArtemDutchak <artemdutchak@gmail.com>
2022-09-15 15:03:55 +0300

    fix translation

--- catalog/language/en-gb/checkout/failure.php
+++ catalog/language/en-gb/checkout/failure.php
@@ -5,7 +5,7 @@ $_['heading_title'] = 'Failed Payment!';
 // Text
 $_['text_basket']   = 'Shopping Cart';
 $_['text_checkout'] = 'Checkout';
-$_['text_failure']  = 'Failed Payment';
+$_['text_failure']  = 'Order processing error';
 $_['text_message']  = '<p>There was a problem processing your payment and the order did not complete.</p>
 
 <p>Possible reasons are:</p>
